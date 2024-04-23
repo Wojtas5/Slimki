707372bcc3ffe3828072965c01350a7fafc10a53
Wojciech <wojtek5552@vp.pl>
2024-04-23 16:16:03 +0200

    Implement CreateNodesQueue method

--- include/huffman.h
+++ include/huffman.h
@@ -2,7 +2,30 @@
 #define HUFFMAN_H
 
 #include <vector>
+#include <queue>
 #include <unordered_map>
+#include "huffman_tree.h"
+
+struct LessFrequency
+{
+    bool operator()(const std::shared_ptr<HuffmanTreeNode>& a,
+                    const std::shared_ptr<HuffmanTreeNode>& b) const
+    {
+        return a->frequency > b->frequency;
+    }
+};
+
+using HuffmanTreeNodePriorityQueue =
+    std::priority_queue<std::shared_ptr<HuffmanTreeNode>,
+        std::vector<std::shared_ptr<HuffmanTreeNode>>,
+        LessFrequency>;
+
+class Huffman
+{
+public:
+    HuffmanTreeNodePriorityQueue CreateNodesQueue(
+        const std::unordered_map<unsigned char, int>& value_frequency_map);
+};
 
 std::unordered_map<unsigned char, int> CountUniqueValueFrequencies(
     const std::vector<unsigned char>& values);
--- tests/huffman_test.cpp
+++ tests/huffman_test.cpp
@@ -12,3 +12,45 @@ TEST(HuffmanTest, ReturnEmptyMapOnEmptyInput)
 {
     ASSERT_TRUE(CountUniqueValueFrequencies({}).empty());
 }
+
+bool AreNodesEqual(const HuffmanTreeNode& node1, const HuffmanTreeNode& node2)
+{
+    return ((node1.value == node2.value) && (node1.frequency == node2.frequency));
+}
+
+bool NodeQueuesEqual(HuffmanTreeNodePriorityQueue queue1,
+    std::queue<std::shared_ptr<HuffmanTreeNode>> queue2)
+{
+    if (queue1.size() != queue2.size())
+    {
+        return false;
+    }
+
+    while (!queue1.empty() && !queue2.empty())
+    {
+        std::shared_ptr<HuffmanTreeNode> node1 = queue1.top();
+        std::shared_ptr<HuffmanTreeNode> node2 = queue2.front();
+        if (!AreNodesEqual(*node1, *node2))
+        {
+            return false;
+        }
+
+        queue1.pop();
+        queue2.pop();
+    }
+
+    return true;
+}
+
+TEST(HuffmanTest, CreatesPriorityQueueOfNodesFromMapSortedBySmallestFrequency)
+{
+    Huffman huffman;
+    std::unordered_map<unsigned char, int> value_frequency_map { { 0, 2 }, { 1, 2 }, { 255, 1 } };
+    std::queue<std::shared_ptr<HuffmanTreeNode>> output_queue;
+
+    output_queue.push(std::make_shared<HuffmanTreeNode>(HuffmanTreeNode { { 255, 1 } }));
+    output_queue.push(std::make_shared<HuffmanTreeNode>(HuffmanTreeNode { { 1, 2 } }));
+    output_queue.push(std::make_shared<HuffmanTreeNode>(HuffmanTreeNode { { 0, 2 } }));
+
+    ASSERT_TRUE(NodeQueuesEqual(huffman.CreateNodesQueue(value_frequency_map), output_queue));
+}
